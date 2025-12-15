b2f8c9c18ae2aa40fc14274406a3a317570927eb
Arthurrmande <arthur.mande@etu.unilim.fr>
2025-12-15 22:15:25 +0100

    ajout page slide horizontale

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import './style.css'
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
+// On sélectionne tous les panneaux qui doivent défiler horizontalement
 const sections = gsap.utils.toArray(".panel");
 
 gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
+  xPercent: -100 * (sections.length - 1), // Déplace vers la gauche
   ease: "none",
   scrollTrigger: {
-    trigger: ".horizontal-scroll",
-    pin: true,
-    scrub: 1,
-    snap: 1 / (sections.length - 1),
+    trigger: ".horizontal-scroll-container", // Élément qui déclenche
+    pin: true,     // Épingle l'écran
+    scrub: 1,      // Fluidité du scroll
+    // Définit la longueur du scroll virtuel
     end: () => "+=" + document.querySelector(".horizontal-wrapper").offsetWidth
   }
 });
