f352b20e1f635dfcb8c7883b01a3db046358b5c7
Arthurrmande <arthur.mande@etu.unilim.fr>
2025-12-15 17:22:57 +0100

    #test css

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+// ... autres imports
+
+export default defineConfig({
+  // Remplace 'nom-de-ton-repo' par le nom exact de ton dépôt sur GitHub
+  base: '/SA-3.Crea.03--main/', 
+  plugins: [
+    // ... tes plugins
+  ],
+})
